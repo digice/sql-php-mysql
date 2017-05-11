87beb0f01ba0f7e8015a66b3e7ad4ff622675903
Roderic Linguri <linguri@digices.com>
2017-05-11 16:24:05 -0600

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,15 @@
+<?php
+
+/** SQL Autoload **/
+
+$sql_dir = __DIR__.DIRECTORY_SEPARATOR.'lib';
+
+$sql_itr = new DirectoryIterator($sql_dir);
+
+foreach ($sql_itr as $item) {
+  $file = $item->getFilename();
+  if (substr($file,0,1) != '.') {
+    $path = $sql_dir.DIRECTORY_SEPARATOR.$file;
+    require_once($path);
+  }
+}
